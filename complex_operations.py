ce25d236f04553f92949005ca5ee8daeeb235b32
riisa <riisa.toro@gmail.com>
2025-01-21 15:23:04 +0200

    feat: implement log and factorial operations

---
+++ complex_operations.py
@@ -0,0 +1,16 @@
+from math import log, e
+
+
+def log_10(val: int) -> float:
+    return log10(val)
+
+
+def ln(val: int) -> float:
+    return log(val, e)
+
+
+def factorial(val: int) -> int
+    if val == 1:
+        return 1
+    
+    return factorial(val - 1) * val
