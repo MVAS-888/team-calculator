f9657482cceabc104066c16451e849e26058ba4e
Olga Budytska <olgabudickaja@gmail.com>
2025-01-21 15:12:42 +0200

    olga's commit: added sin and cos functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 
@@ -16,6 +18,15 @@ def divide(a, b):
 def modulo(a, b):
     return a % b
 
+# OLGA: sin(a)
+def sin_number(a: float) -> float:
+    return math.sin(a)
+
+
+# OLGA: cos(a)
+def cos_number(a: float) -> float:
+    return math.sin(a)
+
 def menu():
     print("\nМеню:")
     print("1. Додавання (+)")
@@ -23,6 +34,8 @@ def menu():
     print("3. Множення (*)")
     print("4. Ділення (/)")
     print("5. Остача від ділення (%)")
+    print("6. Sinus (sin)")
+    print("5. Cosinus (cos)")
     print("0. Вихід")
 
 if __name__ == "__main__":
@@ -32,20 +45,33 @@ if __name__ == "__main__":
         if choice == "0":
             print("Дякуємо за використання калькулятора!")
             break
-        elif choice in ["1", "2", "3", "4", "5"]:
+        elif choice in ["1", "2", "3", "4", "5", "6", "7"]:
             try:
-                num1 = float(input("Введіть перше число: "))
-                num2 = float(input("Введіть друге число: "))
-                if choice == "1":
-                    print("Результат:", add(num1, num2))
-                elif choice == "2":
-                    print("Результат:", subtract(num1, num2))
-                elif choice == "3":
-                    print("Результат:", multiply(num1, num2))
-                elif choice == "4":
-                    print("Результат:", divide(num1, num2))
-                elif choice == "5":
-                    print("Результат:", modulo(num1, num2))
+                if choice in ["6", "7"]:
+                    num = float(input("Введіть число для обчислення: "))
+                    if choice == "6":
+                        print("Результат:", sin_number(num))
+                    elif choice == "7":
+                        print("Результат:", cos_number(num))
+                else:
+                    num1 = float(input("Введіть перше число: "))
+                    num2 = float(input("Введіть друге число: "))
+                    if choice == "1":
+                        print("Результат:", add(num1, num2))
+                    elif choice == "2":
+                        print("Результат:", subtract(num1, num2))
+                    elif choice == "3":
+                        print("Результат:", multiply(num1, num2))
+                    elif choice == "4":
+                        print("Результат:", divide(num1, num2))
+                    elif choice == "5":
+                        print("Результат:", modulo(num1, num2))
+                    elif choice == "5":
+                        print("Результат:", modulo(num1, num2))
+                    elif choice == "6":
+                        print("Результат:", sin_number(num1))
+                    elif choice == "7":
+                        print("Результат:", cos_number(num1))
             except ValueError:
                 print("Помилка: введіть коректне число!")
         else:
