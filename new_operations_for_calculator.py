a432f405da1e50d23b54e8fc9fea342d519e1b4e
riisa <riisa.toro@gmail.com>
2025-01-22 18:07:09 +0200

    ref: convert pure code to functions

--- new_operations_for_calculator.py
+++ new_operations_for_calculator.py
@@ -1,15 +1,9 @@
 import math
 
-x = float(input("Въведете число x: "))
-n = float(input("Въведете степен n: "))
+
+def pow_n(base: int, value: int):
+    return base ** value
 
 
-result_pow = x ** n
-print(f"{x}^{n} = {result_pow}")
-
-
-value = float(input("Въведете число за квадратен корен: "))
-
-
-result_sqrt = math.sqrt(value)
-print(f"sqrt({value}) = {result_sqrt}")
+def square_root(value: int):
+    return math.sqrt(value)
